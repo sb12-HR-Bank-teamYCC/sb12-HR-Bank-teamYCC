8cbf57eb3dd139290843d656a73d1c9af5f4f986
gusdn6763 <gusdn6763@gmail.com>
2026-05-18 18:44:49 +0900

    feat: BaseEntity 수정, DataBackupEntity, BackupDto 추가.
    1. UUID v7을 위한 의존성 추가.
    2. 원활한 상호 협업을 위한 DataBackupEntity, BackupDto 추가
    3. 스케줄러 시간 설정을 위한 설정 파일에 변수 추가.

--- src/main/java/com/codeit/hrbank/entity/backupStatus/DataBackup.java
+++ src/main/java/com/codeit/hrbank/entity/backupStatus/DataBackup.java
@@ -1,7 +1,50 @@
 package com.codeit.hrbank.entity.backupStatus;
 
-import com.codeit.hrbank.entity.base.BaseEntity;
 
-public class DataBackup extends BaseEntity {
+import com.codeit.hrbank.entity.FileMetadata;
+import com.codeit.hrbank.entity.base.BaseUpdatableEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import java.time.Instant;
+import java.util.UUID;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+import org.springframework.data.annotation.CreatedDate;
 
+@Entity
+@Table(name = "data_backups")
+@Getter
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
+public class DataBackup extends BaseUpdatableEntity {
+
+  @Column(name = "worker", length = 45)
+  private String worker;
+
+  @CreatedDate
+  @Column(name = "started_at")
+  private Instant startedAt;
+
+  @Column(name = "ended_at")
+  private Instant endedAt;
+
+  @Enumerated(EnumType.STRING)
+  @Column(name = "status", length = 20)
+  private BackupStatus status;
+
+  //@OneToOne(fetch = FetchType.LAZY)
+  //@JoinColumn(name = "file_id")
+  //private FileMetadata fileMetadata;
+
+  @Column(name = "file_id")
+  private UUID fileMetadata;
 }
