dce5fcffcc4be0102ca57d44c82a6287693102d0
zziyo8 <seok8@outlook.com>
2026-05-19 13:27:12 +0900

    feat: DepartmentDto 및 Mapper 수정
    1. Dto import 누락된 부분 추가
    2. LocalDate → Instant 수정
    3. Mapper 타압 변환 부분 삭제

--- src/main/java/com/codeit/hrbank/dto/department/DepartmentDto.java
+++ src/main/java/com/codeit/hrbank/dto/department/DepartmentDto.java
@@ -1,6 +1,15 @@
 package com.codeit.hrbank.dto.department;
 
 
+import java.time.Instant;
+import java.time.LocalDate;
+import java.util.UUID;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 @Getter
 @Setter
 @NoArgsConstructor
@@ -11,7 +20,7 @@ public class DepartmentDto {
   private String name;
   private String description;
   private LocalDate establishedDate;
-  private LocalDateTime createdAt;
-  private LocalDateTime updatedAt;
+  private Instant createdAt;
+  private Instant updatedAt;
   private int employeeCount;
 }
