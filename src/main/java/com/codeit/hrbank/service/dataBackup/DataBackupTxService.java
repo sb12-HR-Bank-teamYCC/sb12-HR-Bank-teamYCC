4345a69cb37f42a2fc47d127073bfde07fd7f481
gusdn6763 <gusdn6763@gmail.com>
2026-05-20 11:45:49 +0900

    feat: 오류 수정

---
+++ src/main/java/com/codeit/hrbank/service/dataBackup/DataBackupTxService.java
@@ -0,0 +1,28 @@
+package com.codeit.hrbank.service.dataBackup;
+
+import com.codeit.hrbank.entity.backupStatus.BackupStatus;
+import com.codeit.hrbank.entity.backupStatus.DataBackup;
+import com.codeit.hrbank.repository.DataBackupRepository;
+import java.time.Instant;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class DataBackupTxService {
+
+  private final DataBackupRepository dataBackupRepository;
+
+  @Transactional(propagation = Propagation.REQUIRES_NEW)
+  public DataBackup createInProgressBackup(String worker) {
+    return dataBackupRepository.save(
+        DataBackup.builder()
+            .worker(worker)
+            .startedAt(Instant.now())
+            .status(BackupStatus.IN_PROGRESS)
+            .build()
+    );
+  }
+}
