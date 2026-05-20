5c0a41a63516aba4f9edda781bf8c3abab94da25
송유정 <jyooy26@naver.com>
2026-05-20 10:18:06 +0900

    fix : FileConfig 저장경로 수정

--- src/main/java/com/codeit/hrbank/common/config/FileConfig.java
+++ src/main/java/com/codeit/hrbank/common/config/FileConfig.java
@@ -6,23 +6,17 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import lombok.Getter;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
 @Getter
 @Configuration
 public class FileConfig {
 
-    // 저장 경로는 application.yml에서 주입
-    @Value("${hr-bank.storage.local.root-path}")
-    private String storagePath;
     private Path uploadDir;
 
     @PostConstruct
     public void init() {
-
-        uploadDir = Paths.get(storagePath).toAbsolutePath().normalize();
-
+        uploadDir = Paths.get(System.getProperty("user.dir"), "uploads");
         if (Files.notExists(uploadDir)) {
             try {
                 Files.createDirectories(uploadDir);
